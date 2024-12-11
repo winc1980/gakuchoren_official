16a4e2a32aeaeae02333dbff4ab7051deef4e619
Rio <9rio10089@gmail.com>
2024-12-11 19:16:08 +0900

    add config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gakuchoren.github.io',
+  base: '/gakuchoren-hp',
   vite: {
     resolve: {
       alias: {
